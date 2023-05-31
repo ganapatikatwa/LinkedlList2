5482a0c5ff4b41d0f8f2e2ca6cd535af71eaf6d1
Ganapati <ganapatasakatwa@gmail.com>
2023-05-31 07:15:13 +0530

    [GK] Add Node to Linked List

---
+++ LinkedList/Linkedlist.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Linkedlist
+    {
+        Node head;
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp= head;
+                while(temp.Next!= null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} Inserted into LinkedList",node.Data);
+        }
+
+        public void Display()
+        {
+            if(head==null)
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Linked List is Empty");
+                Console.ResetColor();
+            }
+            else
+            {
+                Node temp= head;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Linked List is  Not Empty");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the Choice\n1)Add Node");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Linkedlist linkedList = new Linkedlist();
+                        linkedList.AddNode(56);
+                        linkedList.AddNode(30);
+                        linkedList.AddNode(70);
+                        linkedList.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
+
